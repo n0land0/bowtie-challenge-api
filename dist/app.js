1c9f76b312d815d2c6b3f8dc463f06eda4de5d2f
Nolan Caine <nolancaine@gmail.com>
2022-04-16 16:15:49 -0600

    Compile latest

--- dist/app.js
+++ dist/app.js
@@ -7,5 +7,7 @@ const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
 app.set('port', process.env.PORT || 3001);
-app.use(routes_1.default);
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded());
+app.use('/api/v1', routes_1.default);
 app.listen(app.get('port'), () => console.log(`App is running on ${app.get('port')}`));
