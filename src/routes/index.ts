b14db6dfe0b030cfc0cd7f9e5d8fb4847d5725e1
Nolan Caine <nolancaine@gmail.com>
2022-04-16 15:49:17 -0600

    Install packages and configure directories, linting

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  // res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
