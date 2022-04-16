798b9b1158745fc887061ec3be887c0fd6093b2c
Nolan Caine <nolancaine@gmail.com>
2022-04-16 16:15:10 -0600

    Prepend api route

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.set('port', process.env.PORT || 3001);
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(router);
+app.use('/api/v1', router);
 
 app.listen(app.get('port'), () =>
   console.log(`App is running on ${app.get('port')}`)
