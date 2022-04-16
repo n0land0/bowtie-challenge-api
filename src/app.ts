b143de338f1a0fe0cb8e030e2c8be4c08fab6dbd
Nolan Caine <nolancaine@gmail.com>
2022-04-16 16:10:01 -0600

    Add basic middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import router from './routes';
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(router);
 
 app.listen(app.get('port'), () =>
