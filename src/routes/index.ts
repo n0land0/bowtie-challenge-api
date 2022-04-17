6be7db928572242c5fafc24051269d190c7c821f
Nolan Caine <nolancaine@gmail.com>
2022-04-16 18:52:38 -0600

    Delete boiler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,6 @@ import { NextFunction, Request, Response, Router } from 'express';
 
 const router = Router();
 
-// router.get('/', function (req, res, next) {
-//   // res.render('index', { title: 'Express' });
-// });
-
 router.get('/projects');
 
 export default router;
