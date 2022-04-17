75bca8b713cf7cafb89827fadbfc09d98e692099
Nolan Caine <nolancaine@gmail.com>
2022-04-17 02:06:28 -0600

    Update controller

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
-import { createNewTodo, getAllTodosByProject } from '../controllers/todo';
+import {
+  createNewTodo,
+  getAllTodosByProject,
+  updateTodo,
+} from '../controllers/todo';
 
 const todoRouter = Router();
 
 todoRouter.get('/projects/:projectId/todos', getAllTodosByProject);
 todoRouter.post('/projects/:projectId/todos', createNewTodo);
+todoRouter.patch('/projects/:projectId/todos/:todoId', updateTodo);
 
 export default todoRouter;
