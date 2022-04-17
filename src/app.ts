7d2bf4b6d2349d7b8d7ee92b8c051eac4186abef
Nolan Caine <nolancaine@gmail.com>
2022-04-16 20:01:40 -0600

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 
 import Project from './models/Project';
@@ -5,11 +6,13 @@ import router from './routes';
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/api/v1', router);
 
-Project.fetchAll().then((response) => console.log(response));
+// Project.fetchAll().then((response) => console.log(response));
 
 app.listen(app.get('port'), () =>
   console.log(`App is running on ${app.get('port')}`)
