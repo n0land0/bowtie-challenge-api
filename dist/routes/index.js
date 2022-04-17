6dfc989457b1667a4caa853165d85aa4d3f80ad9
Nolan Caine <nolancaine@gmail.com>
2022-04-16 18:53:08 -0600

    Compile latest

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -2,8 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
-// router.get('/', function (req, res, next) {
-//   // res.render('index', { title: 'Express' });
-// });
 router.get('/projects');
 exports.default = router;
