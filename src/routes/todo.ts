6dad12f3889afcbb037d481ef166337fde9f60bc
Nolan Caine <nolancaine@gmail.com>
2022-04-17 22:03:20 -0600

    Add delete all todos in project route

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import {
   createNewTodo,
+  deleteAllTodosInProject,
   deleteTodo,
   getAllTodosByProject,
   updateTodo,
@@ -13,5 +14,6 @@ todoRouter.get('/projects/:projectId/todos', getAllTodosByProject);
 todoRouter.post('/projects/:projectId/todos', createNewTodo);
 todoRouter.patch('/projects/:projectId/todos/:todoId', updateTodo);
 todoRouter.delete('/projects/:projectId/todos/:todoId', deleteTodo);
+todoRouter.delete('/projects/:projectId/todos', deleteAllTodosInProject);
 
 export default todoRouter;
