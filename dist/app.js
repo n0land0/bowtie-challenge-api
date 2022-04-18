b7bfacd30f88f40c56b3466808df05d5f05b8453
Nolan Caine <nolancaine@gmail.com>
2022-04-18 02:40:48 -0600

    Add extended option

--- dist/app.js
+++ dist/app.js
@@ -11,7 +11,7 @@ const app = (0, express_1.default)();
 app.set('port', process.env.PORT || 3001);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(express_1.default.urlencoded());
+app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/api/v1', project_1.default);
 app.use('/api/v1', todo_1.default);
 app.listen(app.get('port'), () => console.log(`App is running on ${app.get('port')}`));
